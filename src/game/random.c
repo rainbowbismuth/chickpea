421b24a93f187e7be0d39011e684b87e0aa1c07c
Emily A. Bellows <emily.a.bellows@gmail.com>
2020-07-25 22:41:46 -0400

    random.c: i like this better

--- src/game/random.c
+++ src/game/random.c
@@ -1,21 +1,18 @@
-#include "chickpea.h"
 #include "game/random.h"
 
-static uint32_t init_counter = 0xEE2AD2F;
-static struct random global_random_state = { 0 };
+static struct random global_random = { .a = 0xE54D9699,
+				       .b = 0x5FA3B2BC,
+				       .c = 0x52B5A326,
+				       .d = 0xBFD2A478,
+				       .counter = 0x56DA3F0B };
 
-/*
- * TODO: Revisit this initialization, especially when I get
- *   timers going, maybe?
- */
 void random_init(struct random *random)
 {
-	init_counter += 0x915C8C73;
-	random->a = 0xE54D9699 ^ init_counter;
-	random->b = 0x5FA3B2BC ^ init_counter;
-	random->c = 0x52B5A326 ^ init_counter;
-	random->d = 0xBFD2A478 ^ init_counter;
-	random->counter = 0x56DA3F0B ^ init_counter;
+	random->a = random_global();
+	random->b = random_global();
+	random->c = random_global();
+	random->d = random_global();
+	random->counter = random_global();
 }
 
 uint32_t random_next(struct random *random)
@@ -38,8 +35,5 @@ uint32_t random_next(struct random *random)
 
 uint32_t random_global(void)
 {
-	if (global_random_state.counter == 0) {
-		random_init(&global_random_state);
-	}
-	return random_next(&global_random_state);
+	return random_next(&global_random);
 }
