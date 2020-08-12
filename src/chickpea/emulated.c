b4cd9327a9c661357b423e9adfe757bb50b3e4ec
Emily A. Bellows <emily.a.bellows@gmail.com>
2020-08-12 15:06:55 -0400

    emulated.c: SDL_Init change, to be pedantic

--- src/chickpea/emulated.c
+++ src/chickpea/emulated.c
@@ -82,7 +82,7 @@ int main(int argc, const char *nonnull argv[])
 		return any_failures;
 	}
 
-	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK);
+	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER);
 	atexit(SDL_Quit);
 	window = SDL_CreateWindow("chickpea-c", SDL_WINDOWPOS_CENTERED,
 				  SDL_WINDOWPOS_CENTERED, real_win_width,
